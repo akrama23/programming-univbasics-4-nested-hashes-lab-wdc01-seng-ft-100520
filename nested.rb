6af0b0de2713eec6a580253842854e7d06ad48ec
akam alam <arundelfootball23@gmail.com>
2020-09-25 03:59:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -75,6 +75,8 @@ def adding_matz
 	programmer_hash = 
  		{
  		  :yukihiro_matsumoto => {
+ 		    :known_for =>{}
+ 		    :languages => {}
  		    
  		  },
  		  
@@ -91,8 +93,8 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}, 
- {:languages => ["LISP", "C"]}
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash{:yukihiro_matsumoto][:languages]= ["LISP", "C"]
 
 end
 
